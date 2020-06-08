223089bc8e256ec249270dda7ab7281ac47ca3c5
Uriah Harston <uharston@gmail.com>
2020-06-08 20:37:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,6 +32,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
+    binding.pry
     # m.title = title
     # m.release_date = release_date
   end

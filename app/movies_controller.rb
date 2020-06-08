cfcf52839c089142e823b57cb7009b87f1f34a87
Uriah Harston <uharston@gmail.com>
2020-06-08 20:44:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,8 +38,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  binding.pry
-  __
+
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database

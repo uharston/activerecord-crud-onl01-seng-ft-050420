5d0713678629d7540f06798023a7b704b74f7fc1
Uriah Harston <uharston@gmail.com>
2020-06-08 20:13:03 +0000

    Automatically backed up by Learn

--- db/migrate/20200608201101_create_movies.rb
+++ db/migrate/20200608201101_create_movies.rb
@@ -1,10 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    create_table movies do |m| 
-      m.string :title 
-      m.integer :release_date 
+    create_table movies do |m|
+      m.string :title
+      m.integer :release_date
       m.string :director
-      m.string :lead 
+      m.string :lead
       m.boolean :in_theaters
   end
 end

92609f66abd621239a6a7988643d394e1754a6a0
Uriah Harston <uharston@gmail.com>
2020-06-08 20:50:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,8 +74,6 @@ def can_be_found_updated_and_saved
   movie = Movie.find_by(title: "Awesome Flick")
   movie.title = "Even Awesomer Flick"
   movie.save
-  __
-  __
 end
 
 def can_update_using_update_method

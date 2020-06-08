8ff6454398a1d218be8e2a4a1b85e9253d29bc4d
Uriah Harston <uharston@gmail.com>
2020-06-08 20:46:08 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -43,15 +43,15 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.all.order(id: :desc).last.title
 end
 
 def can_find_by_multiple_attributes

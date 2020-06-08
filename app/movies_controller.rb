0bc89ccfc13c8964e1c37f8d56e37417315f14b9
Uriah Harston <uharston@gmail.com>
2020-06-08 20:28:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Replace the '__' in the below methods to to make the specs pass!
 # Each '__' corresponds to a single line of code you will need to write.
 # See the example below on the left and how it should look on the right.
